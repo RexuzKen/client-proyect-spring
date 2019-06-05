23527fed22ea19951ef24030d3841f162b7fbc00
Togashi <togashiken99@gmail.com>
2019-06-05 19:14:31 -0300

    Inicio del proyecto

---
+++ src/main/java/clientproyectspring/clientproyectspring/ClientProyectSpringApplication.java
@@ -0,0 +1,13 @@
+package clientproyectspring.clientproyectspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientProyectSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientProyectSpringApplication.class, args);
+	}
+
+}
