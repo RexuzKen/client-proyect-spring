438b9e842144a6d5a118bd6e8c8a46882bdbea6e
Togashi <togashiken99@gmail.com>
2019-06-05 23:14:14 -0300

    version 1 api rest cliente terminada

--- src/main/java/clientproyectspring/clientproyectspring/controllers/ClientResource.java
+++ src/main/java/clientproyectspring/clientproyectspring/controllers/ClientResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import clientproyectspring.clientproyectspring.models.entity.Client;
@@ -30,6 +33,7 @@ public class ClientResource {
 	}
 	
 	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
 	public Client save(@Valid @RequestBody Client client) {
 		return clientService.save(client);
 	}
